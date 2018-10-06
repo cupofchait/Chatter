4894137d610686fe5234dde19a96a4a15ca8d4ec
Nicole Miller <cupofchait@gmail.com>
2018-10-06 14:50:13 -0500

    blahblah4

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,25 +4,16 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
-(defroutes app-routes
-  (GET "/" []
-    (page/html5
-      [:head
-       [:title "chatter"]]
-      [:body
-       [:h1 "Our Chat App"]]))
-  (route/not-found "Not Found"))
-
+            (defn generate-message-view
+              "This generates the HTML for displaying messages"
+              []
+              (page/html5
+               [:head
+                [:title "chatter"]]
+               [:body
+                [:h1 "Our Chat App"]]))
+            (defroutes app-routes
+              (GET "/" [] (generate-message-view))
+              (route/not-found "Not Found"))
 (def app
   (wrap-defaults app-routes site-defaults))
-  (defn generate-message-view
-    "This generates the HTML for displaying messages"
-    []
-    (page/html5
-     [:head
-      [:title "chatter"]]
-     [:body
-      [:h1 "Our Chat App"]]))
-  (defroutes app-routes
-    (GET "/" [] (generate-message-view))
-    (route/not-found "Not Found"))
